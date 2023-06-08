4571c8ca82735bdbabdb7d98eb66c6f7294d6b52
Konstantin <zkvprog@gmail.com>
2023-06-08 16:23:46 +0300

    Fixed paginate tag page

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -9,7 +9,7 @@ class TagsController extends Controller
 {
     public function index(Tag $tag)
     {
-        $articles = $tag->articles()->with('tags')->get();
+        $articles = $tag->articles()->with('tags')->simplePaginate(10);;
 
         return view('articles.index', compact('articles'));
     }
